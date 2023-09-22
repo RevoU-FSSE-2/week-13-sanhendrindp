dfb399e430d5a592a8fe1f4e24f61b3eee62f3d5
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-22 18:39:13 +0700

    fix: :bug: Using manual input token, POST request to add category fix

--- react-app/src/containers/CategoryNew/index.tsx
+++ react-app/src/containers/CategoryNew/index.tsx
@@ -11,7 +11,11 @@ const CategoryNew = () => {
         "https://mock-api.arikmpt.com/api/category/create",
         {
           method: "POST",
-          headers: { "Content-Type": "application/json" },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg1YzIwMjM3LTkyN2QtNGNjZi1iZDUyLWQ1NGE2Y2Y5ZWE3MCIsImlhdCI6MTY5NTM3NDMyMywiZXhwIjoxNjk1Mzk1OTIzfQ.wxmY-3yYHMf2pxhelTKkbqxviVspTbcL6qDvMVFJdDA",
+          },
           body: JSON.stringify(values),
         }
       );
