78ff5d143a896304ab04f4cb024b8ba8056a03db
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-28 21:01:33 +0700

    test: :test_tube: Create testing for LoginForm component

---
+++ react-app/src/components/LoginForm/LoginForm.test.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import { render } from "@testing-library/react";
+import LoginForm from ".";
+import { MemoryRouter } from "react-router-dom"; // Used MemoryRouter to wrap <LoginForm/> because i used useNavigate in LoginForm
+
+describe("test login form", () => {
+  const mockProps = () => {};
+  it("title render correctly", async () => {
+    render(
+      <MemoryRouter>
+        <LoginForm onSubmit={mockProps} />
+      </MemoryRouter>
+    );
+  });
+});
